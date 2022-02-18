26f84f76291f4131f9f54a4c027ae3d065f650b3
lavinia <lavinialuiz16@gmail.com>
2022-02-18 15:06:20 -0300

    exclusão do aquivo dados.json e adição da rota get/

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var Usuario = require('./model/Usuario');
+var Usuario = require("./model/Usuario");
 
 var path = require("path");
 
@@ -8,44 +8,47 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: false }));
 
+app.get("/", function (req, res) {
+  console.log("Oi Mundo!");
+});
+
 app.get("/add", function (req, res) {
   res.render("index.ejs", {});
 });
 
-app.post('/add', function (req, res){
-   var usuario = new Usuario ({
-     nome: req.body.nome,
-     email: req.body.email,
-     senha: req.body.senha,
-     foto: req.body.foto
-   })
+app.post("/add", function (req, res) {
+  var usuario = new Usuario({
+    nome: req.body.nome,
+    email: req.body.email,
+    senha: req.body.senha,
+    foto: req.body.foto,
+  });
 
-   usuario.save(function (err, docs){
-     if(err){
-       req.send("Aconteceu o seguinte erro: " +err);
-     }else{
-       res.send(docs.nome+" foi salvo com sucesso!!");
-     }
-   });
+  usuario.save(function (err, docs) {
+    if (err) {
+      req.send("Aconteceu o seguinte erro: " + err);
+    } else {
+      res.send(docs.nome + " foi salvo com sucesso!!");
+    }
+  });
 });
 
-app.get("/", function (req,res){
-  Usuario.find({}).then(function(docs){ 
-  res.render("list.ejs", {Usuarios: docs});
+app.get("/", function (req, res) {
+  Usuario.find({}).then(function (docs) {
+    res.render("list.ejs", { Usuarios: docs });
+  });
 
-});
+  app.get("/del/:id", function (req, res) {
+    Usuario.findByIdAndDelete(req.params.id, function (err, doc) {
+      if (err) {
+        res.send("Aconteceu o seguinte erro: ", +err);
+      } else {
+        res.redirect("/");
+      }
+    });
+  });
 
-app.get('/del/:id', function(req, res){
- Usuario.findByIdAndDelete(req.params.id, function(err,doc){
-   if(err){
-     res.send("Aconteceu o seguinte erro: ", +err);
-   }else{
-     res.redirect("/");
-   }
- });
-});
-
-app.listen("3000", function () {
-  console.log("O servidor foi iniciado na porta 3000");
-})
+  app.listen("3000", function () {
+    console.log("O servidor foi iniciado na porta 3000");
+  });
 });
